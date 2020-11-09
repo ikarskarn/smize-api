02dfb20a3894ed3fded7eadbb82a52b3f03ce147
Marc Tucker <marcttucker@gmail.com>
2020-11-09 17:48:13 -0500

    setup local database

---
+++ seeds/seeds.smize_categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO smize_categories (title)
+VALUES
+    ('Women'),
+    ('Men'),
+    ('Kids');
---
+++ seeds/seeds.smize_sayings.sql
@@ -0,0 +1,11 @@
+INSERT INTO smize_sayings (category_id, saying_content)
+VALUES
+    (3, 'Pretend you are a Storm Trooper.'),
+    (3, 'Now slightly freeze. -No. You’re completely frozen. Just freeze slightly.'),
+    (3, 'Look to your East. No that was West.'),
+    (2, 'Don&#39;t look so miserable. Less Misery!'),
+    (2,'Think about asphalt.'),
+    (2,'Think about Chewbacca.'),
+    (1,'Give me a face like you are delighted.'),
+    (1,'Now relax your nose for me.'),
+    (1,'Think about Junior Mints.');
