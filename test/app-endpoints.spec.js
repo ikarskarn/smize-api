04c3845784688184c4ee97bf48a4b0f565c2c7e6
Marc Tucker <marcttucker@gmail.com>
2020-11-14 00:37:24 -0500

    added post and delete tests, updated README

--- test/app-endpoints.spec.js
+++ test/app-endpoints.spec.js
@@ -1,4 +1,7 @@
+const { expect } = require("chai");
 const knex = require("knex");
+const supertest = require("supertest");
+const { request, post } = require("../src/app");
 const app = require("../src/app");
 
 describe("Category Endpoints", function () {
@@ -36,6 +39,37 @@ describe("Category Endpoints", function () {
                     .get(`/api/sayings/${sayingId}`)
                     .expect(404, { error: { message: `Saying doesn't exist` } });
             });
+        });
+    });
+
+    describe(`POST /api/sayings`, () => {
+        let data = {
+            id: "1234567",
+            category_id: "1",
+            saying_content: "more funny things",
+        };
+        it("responds with 201 created", (done) => {
+            supertest(app)
+                .post("/api/sayings")
+                .send(data)
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(201)
+                .end((err) => {
+                    if (err) return done(err);
+                    done();
+                });
+        });
+    });
+
+    describe("DELETE /api/sayings/:saying_id", () => {
+        it("It responds with 204", async () => {
+            const newSaying = await supertest(app).post("/api/sayings").send({
+                category_id: 1,
+                saying_content: "Another Funny Thing",
+            });
+            const removedSaying = await supertest(app).delete(`/api/sayings/${newSaying.body.id}`);
+            expect(204);
         });
     });
 });
